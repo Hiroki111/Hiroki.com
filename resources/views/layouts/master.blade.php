669d5f9a04871f4d81f5ce30cbdce91901a1e7b0
Hiroki <mitsuke1116@gmail.com>
2016-01-10 21:47:39 +1000

    The logo is done

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,29 +1,40 @@
 <html>
     <head>
-       <title>@yield('Hiroki.com')</title>
+        <title>@yield('Hiroki.com')</title>
+        <link href="{{asset('css/main.css')}}" rel="stylesheet">
+        
     </head>
     <body>
     <header>
-        <div class = header_logo>
-            <h2>Hiroki.com</h2>
+        <div id = header_image>
+            <h1 id = header_title>Hiroki.com</h1>
         </div>
-        <div class = header_submenu>
-            <ul = submenu>
+        <div id = header_submenu>
+            <ul id = submenu>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
                 <li><a href="#">Links</a></li>
             </ul>
         </div>
-
     </header>
-
-        @section('sidebar')
-            This is the master sidebar.
-        @show
-
+   <div class = sidebar>
+        <ul id = sidebar_genre>
+            <li><a href="#">Genre</a></li>
+        </ul>
+       <ul id = sidebar_popular>
+           <li><a href="#">Popular Articles</a></li>
+       </ul>
+       <ul id = sidebar_link>
+           <li><a href="#">Links</a></li>
+       </ul>
+   </div>
     <div class="container">
         @yield('content')
     </div>
+
+    <footer>
+        <p>Hiroki.com - Established in 2016</p>
+    </footer>
     </body>
 </html>
