8bcd00925d583fdeb869664fabcffae33c13aaa4
Hiroki <mitsuke1116@gmail.com>
2016-01-11 18:22:02 +1000

    add submenu

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,21 +2,27 @@
     <head>
         <title>@yield('Hiroki.com')</title>
         <link href="{{asset('css/main.css')}}" rel="stylesheet">
-        
+
     </head>
     <body>
     <header>
         <div id = header_image>
             <h1 id = header_title>Hiroki.com</h1>
         </div>
-        <div id = header_submenu>
-            <ul id = submenu>
+        <nav>
+            <ul>
                 <li><a href="#">Home</a></li>
+                <li  class = has_sub><a href="#">Travel Tips</a>
+                    <ul>
+                        <li><a href='#'>Tip 1</a></li>
+                        <li class='last'><a href='#'>Tip 2</a></li>
+                    </ul>
+                </li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
                 <li><a href="#">Links</a></li>
             </ul>
-        </div>
+        </nav>
     </header>
    <div class = sidebar>
         <ul id = sidebar_genre>
