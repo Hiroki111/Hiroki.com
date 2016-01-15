c72ab7d27fe48e0562eee941b310df0387883459
Hiroki <mitsuke1116@gmail.com>
2016-01-15 22:44:23 +1000

    add text areas to post_article

--- resources/views/edit_pages/post_article.blade.php
+++ resources/views/edit_pages/post_article.blade.php
@@ -8,9 +8,14 @@
         <header>Post new articles</header>
         <div id = content>
             <p>Title</p>
+            <textarea style = "width:100%;"></textarea>
             <p>content</p>
+            <textarea style="width:100%; height:400px;"></textarea>
         </div>
-        <div id = result></div>
+        <div id = result>
+            <p>result</p>
+        </div>
+        <footer>see the result</footer>
     </body>
 
 </html>
